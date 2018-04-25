f26f134907ebc1c0d250b84fa34e938b8ae68e14
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-25 13:03:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+
+  def initialize(bark)
+    @bark = bark
+  end
+
+  def bark
+    @bark
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  
